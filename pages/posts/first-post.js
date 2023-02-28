e382605aaeecac2fe335f55446c4b18b490cca09
muchirasamwel <muchirasamwel@gmail.com>
2023-02-28 12:54:37 +0300

    * Play with css styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,37 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
+import Script from 'next/script'
 import React from 'react'
+import pageStyles from '../../styles/first-post.module.css'
 
 const FirstPost = () => {
   return (
-    <div>
-      <div>First Post</div>
+    <div className={pageStyles.container}>
       <div>
-        <Link href={'/'}>Back to home</Link>
+        <Head>
+          <title>Posts head</title>
+        </Head>
+        <Script
+          src='https://connect.facebook.net/en_US/sdk.js'
+          strategy='lazyOnload'
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <main>
+          <Image
+            className={pageStyles.image}
+            src={'/deb.png'}
+            // src={'https://random.imagecdn.app/500/500'}
+            width={200}
+            height={50}
+          />
+          <div className={pageStyles.heading}>First Post</div>
+          <div className={pageStyles.body}>
+            <Link href={'/'}>Back to home</Link>
+          </div>
+        </main>
       </div>
     </div>
   )
